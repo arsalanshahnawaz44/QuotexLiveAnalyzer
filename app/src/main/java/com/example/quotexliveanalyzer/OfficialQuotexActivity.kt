73775b70f6d6999a28cd83f55a917a5c7c6d9384
Arsalan Shahnawaz <arsalanshahnawaz44@users.noreply.github.com>
2026-09-16 19:53:50 +0400

    Fix Quotex WebView Kotlin syntax

--- app/src/main/java/com/example/quotexliveanalyzer/OfficialQuotexActivity.kt
+++ app/src/main/java/com/example/quotexliveanalyzer/OfficialQuotexActivity.kt
@@ -25,33 +25,6 @@ class OfficialQuotexActivity : Activity() {
 
         webView.loadUrl("https://quotex.io/")
 
-package com.example.quotexliveanalyzer
-
-import android.app.Activity
-import android.os.Bundle
-import android.webkit.WebChromeClient
-import android.webkit.WebView
-import android.webkit.WebViewClient
-
-class OfficialQuotexActivity : Activity() {
-
-    private lateinit var webView: WebView
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        webView = WebView(this)
-
-        webView.settings.javaScriptEnabled = true
-        webView.settings.domStorageEnabled = true
-        webView.settings.databaseEnabled = true
-        webView.settings.mediaPlaybackRequiresUserGesture = false
-
-        webView.webViewClient = WebViewClient()
-        webView.webChromeClient = WebChromeClient()
-
-        webView.loadUrl("https://quotex.io/")
-
         setContentView(webView)
     }
 
@@ -63,4 +36,3 @@ class OfficialQuotexActivity : Activity() {
         }
     }
 }
-
